8d4fda3f606380a96ad0969846097cbe555f202b
Lucas <luqasmagra@gmail.com>
2023-01-05 22:52:47 -0300

    work commit

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -10,7 +10,7 @@ const Work = () => {
           <p className="text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600">
             Trabajos
           </p>
-          <p className="py-6">// Algunos de mis proyectos recientes</p>
+          <p className="py-6">//Algunos de mis proyectos recientes</p>
         </div>
         <div className="grid sm:grid-cols-2 md:grid-cols-2 gap-4">
           {projects.map((item, index) => (
